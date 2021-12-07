33a7879b22bada0c3332df6ce6e18d719f741918
YiyuanShang <yiyuan.shang@mail.mcgill.ca>
2021-12-07 17:49:58 -0500

    initial commit

---
+++ src/main/java/com/athensoft/prototype/PrototypeSpringOauth2Application.java
@@ -0,0 +1,13 @@
+package com.athensoft.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeSpringOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeSpringOauth2Application.class, args);
+	}
+
+}
